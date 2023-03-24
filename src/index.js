dfa0d668f3f29f6970c5e1bf29b2c3adcac619bb
José Auyón <83108711+auyjos@users.noreply.github.com>
2023-03-24 17:31:36 -0600

    Archivos. Implementación de bootstrap, webpack
    Se añadió la lógica de js, creación de elementos html con js y webpack.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+import {crearPagIndex} from '../src/appIndex.js'
+import {crearPag} from '../src/Paginas/Librospopulares/pop.js'
+import '../src/Paginas/Librospopulares/stylespop.sass';
+import '../src/Paginas/Nuevoslanzamientos/styleshistoria.sass';
+import '../src/Paginas/Nuevoslanzamientos/stylesnuevos.css'
+//import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap';
+
+// Scripts - App
+import '../src/Paginas/Librospopulares/pop.js';
+import '../src/Paginas/Nuevoslanzamientos/lanzamientos.js'
+import '../src/appIndex.js';
+import '../src/extra.js'
+import '../src/Paginas/Nuevoslanzamientos/historia.js'
+
+import '../src/index.css'
+
+crearPagIndex();
+  
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+   index: './src/index.js',
+   appIndex : './src/appIndex.js',
+   pop: './src/Paginas/Librospopulares/pop.js',
+   lanzamientos: '/src/Paginas/Nuevoslanzamientos/lanzamientos.js',
+   extra : './src/extra.js',
+   historia: './src/Paginas/Nuevoslanzamientos/historia.js',
+   myTemplate: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css", 
+    }), 
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['appIndex']
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/extra.html',
+        filename: 'extra.html',
+       chunks: ['extra']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/Paginas/Librospopulares/libros-populares.html',
+        filename: 'libros-populares.html',
+        chunks:['pop']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/Paginas/Nuevoslanzamientos/nuevos-lanzamientos.html',
+        filename: 'nuevos-lanzamientos.html',
+        chunks: ['lanzamientos']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/Paginas/Nuevoslanzamientos/historia.html',
+        filename: 'historia.html',
+       chunks: ['historia']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/Paginas/Nuevoslanzamientos/historia1.html',
+        filename: 'historia1.html',
+       chunks: ['historia']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/Paginas/Nuevoslanzamientos/historia2.html',
+        filename: 'historia2.html',
+       chunks: ['historia']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/Paginas/Nuevoslanzamientos/historia3.html',
+        filename: 'historia3.html',
+        chunks: ['historia']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/Paginas/Nuevoslanzamientos/historia4.html',
+        filename: 'historia4.html',
+        chunks: ['historia']
+      }),
+],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+           "css-loader",
+           "sass-loader"]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+           "css-loader"
+          ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
